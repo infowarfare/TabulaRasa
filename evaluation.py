680b37359860df095692336712cb020a6ae83550
infowarfare <infowarfare@web.de>
2025-10-23 23:35:25 +0200

    refactor: split between plaintiff and defendant for evaluation

--- evaluation.py
+++ evaluation.py
@@ -8,11 +8,12 @@ with open("dokument.json", "r", encoding="utf-8") as file:
 plaintiff_list = []
 defendant_list = []
 
-plaintiff_dict = dict.fromkeys["Kläger-Vortrag", "Kläger-Passage"]
-defendant_dict = dict.fromkeys["Beklagter-Vortrag", "Beklagter-Passage"]
-
 # iterate through list
 for dict in doc:
+
+    # create empty dicts
+    plaintiff_dict = dict.fromkeys["Kläger-Vortrag", "Kläger-Passage"]
+    defendant_dict = dict.fromkeys["Beklagter-Vortrag", "Beklagter-Passage"]
     
     # get relevant keys
     # kläger-vortrag - kläger-dokument passage
@@ -23,12 +24,27 @@ for dict in doc:
     if argument_plaintiff != ' - ' and document_passage_plaintiff != ' - ':
         print(f"Klägervortrag: {argument_plaintiff} : Passage: {document_passage_plaintiff}")
 
+        # assign filtered values to empty dict
+        plaintiff_dict["Kläger-Vortrag"] = argument_plaintiff
+        plaintiff_dict["Kläger-Passage"] = document_passage_plaintiff
+
+        plaintiff_list.append(plaintiff_dict)
+
+
     # beklagter-vortrag - beklagter-dokument passage
     argument_defendant = dict['Beklagter-Vortrag']
     document_passage_defendant = dict['Beklagter-Dokument Passage']
 
     if argument_defendant != ' - ' and document_passage_defendant != ' - ':
         print(f"Beklagtervortrag: {argument_defendant} : Passage {document_passage_defendant}")
+
+        # assign filtered values to empty dict
+        defendant_dict["Kläger-Vortrag"] = argument_defendant
+        defendant_dict["Kläger-Passage"] = document_passage_defendant
+
+        plaintiff_list.append(defendant_dict)
+
+    
 
     
 
