7614a4a889fad06b09940d499c49f2f48b30f12e
infowarfare <infowarfare@web.de>
2025-10-23 23:44:58 +0200

    fix: assign value to right keys

--- evaluation.py
+++ evaluation.py
@@ -12,8 +12,8 @@ defendant_list = []
 for d in doc:
 
     # create empty dicts
-    plaintiff_dict = dict.fromkeys["Kläger-Vortrag", "Kläger-Passage"]
-    defendant_dict = dict.fromkeys["Beklagter-Vortrag", "Beklagter-Passage"]
+    plaintiff_dict = dict.fromkeys(["Kläger-Vortrag", "Kläger-Passage"])
+    defendant_dict = dict.fromkeys(["Beklagter-Vortrag", "Beklagter-Passage"])
     
     # get relevant keys
     # kläger-vortrag - kläger-dokument passage
@@ -22,7 +22,7 @@ for d in doc:
 
     # only existing arguments and corresponding passages
     if argument_plaintiff != ' - ' and document_passage_plaintiff != ' - ':
-        print(f"Klägervortrag: {argument_plaintiff} : Passage: {document_passage_plaintiff}")
+        #print(f"Klägervortrag: {argument_plaintiff} : Passage: {document_passage_plaintiff}")
 
         # assign filtered values to empty dict
         plaintiff_dict["Kläger-Vortrag"] = argument_plaintiff
@@ -36,15 +36,18 @@ for d in doc:
     document_passage_defendant = d['Beklagter-Dokument Passage']
 
     if argument_defendant != ' - ' and document_passage_defendant != ' - ':
-        print(f"Beklagtervortrag: {argument_defendant} : Passage {document_passage_defendant}")
+        #print(f"Beklagtervortrag: {argument_defendant} : Passage {document_passage_defendant}")
 
         # assign filtered values to empty dict
-        defendant_dict["Kläger-Vortrag"] = argument_defendant
-        defendant_dict["Kläger-Passage"] = document_passage_defendant
+        defendant_dict["Beklagter-Vortrag"] = argument_defendant
+        defendant_dict["Beklagter-Passage"] = document_passage_defendant
 
         defendant_list.append(defendant_dict)
 
-    
+
+print("###### BEKLAGTER ######")
+for d in defendant_list:
+    print(d)
 
     
 
